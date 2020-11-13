7665ed4a75ee8c39ddfbeb52d56d06385d4e1c3e
Gaspor <gaspor991@gmail.com>
2020-11-13 17:58:55 -0300

    Hotfix

--- app/src/main/java/com/example/pitch/doencas/CovidActivity.kt
+++ app/src/main/java/com/example/pitch/doencas/CovidActivity.kt
@@ -9,7 +9,6 @@ import kotlinx.android.synthetic.main.activity_covid.*
 import kotlinx.android.synthetic.main.fragment_doencas.*
 
 class CovidActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_covid)
