8681383848f896113939a71c8e07a8effd3de8c5
LucasFilgueiras <lucks_fil@hotmail.com>
2020-11-14 17:39:29 -0300

    coisas extras

--- app/src/main/java/com/example/pitch/questionario.kt
+++ app/src/main/java/com/example/pitch/questionario.kt
@@ -1,7 +1,12 @@
 package com.example.pitch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.example.pitch.doencas.*
+import kotlinx.android.synthetic.main.activity_inicial.*
 import kotlinx.android.synthetic.main.activity_questionario.*
 import java.lang.StringBuilder
 
@@ -19,6 +24,8 @@ class questionario : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_questionario)
+
+        var intent = Intent(this,questionario::class.java)
 
         resultado_questionario.setOnClickListener {
             if (tosse.isChecked) {
@@ -91,40 +98,53 @@ class questionario : AppCompatActivity() {
                 val resultString19 = StringBuilder()
                 resultString19.append("É provável que você tenha Dengue, Zika ou Chikungunya")
                 textDoenca.text = resultString19.toString()
+                botao_informacao.visibility = View.VISIBLE
+                intent = Intent(this,DengueActivity::class.java)
             }
 
             else if (contadorH1N1 >= 6 && contadorCovid < 9) {
                 val resultString22 = StringBuilder()
                 resultString22.append("É provável que você tenha Gripe comum ou H1N1")
                 textDoenca.text = resultString22.toString()
+                botao_informacao.visibility = View.VISIBLE
+                intent = Intent(this,GripeActivity::class.java)
             }
 
             else if (contadorTubercoluse >= 4 && contadorCovid < 9) {
                 val resultString25 = StringBuilder()
                 resultString25.append("É provável que você tenha Tuberculose")
                 textDoenca.text = resultString25.toString()
+                botao_informacao.visibility = View.VISIBLE
+                intent = Intent(this,TuberculoseActivity::class.java)
             }
 
             else if (contadorAids >= 6 && contadorCovid < 9) {
                 val resultString28 = StringBuilder()
                 resultString28.append("É provável que você tenha Aids")
                 textDoenca.text = resultString28.toString()
-            }
-
-            else if (contadorCovid >= 9) {
-                val resultString16 = StringBuilder()
-                resultString16.append("É provável que você tenha Covid-19")
-                textDoenca.text = resultString16.toString()
+                botao_informacao.visibility = View.VISIBLE
+                intent = Intent(this,AidsActivity::class.java)
             }
 
             else if (contadorHanseniase >= 2 && contadorCovid < 9) {
                 val resultString29 = StringBuilder()
                 resultString29.append("É provável que você tenha Hanseníase")
                 textDoenca.text = resultString29.toString()
+                botao_informacao.visibility = View.VISIBLE
+                intent = Intent(this,HanseniaseActivity::class.java)
+            }
+
+            else if (contadorCovid >= 9) {
+                val resultString16 = StringBuilder()
+                resultString16.append("É provável que você tenha Covid-19")
+                textDoenca.text = resultString16.toString()
+                botao_informacao.visibility = View.VISIBLE
+                intent = Intent(this,CovidActivity::class.java)
             }
 
             else {
                 textDoenca.text = "Inconclusivo"
+                botao_informacao.visibility = View.INVISIBLE
             }
 
             contadorCovid = 0
@@ -132,7 +152,10 @@ class questionario : AppCompatActivity() {
             contadorTubercoluse = 0
             contadorH1N1 = 0
             contadorDengue = 0
-
+            contadorHanseniase = 0
+        }
+        botao_informacao.setOnClickListener {
+            startActivity(intent)
         }
     }
 }
